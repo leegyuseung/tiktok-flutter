5b6faf43f57e3b12802e3b50e979b54b27a129bb
lee gyuseung <gyus0421@gmail.com>
2024-03-20 21:49:53 +0900

    Authentication : Sign Up Form (파일오류)

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tictok_clone/constants/gaps.dart';
 import 'package:tictok_clone/constants/sizes.dart';
+import 'package:tictok_clone/features/authentication/email_screen.dart';
 import 'package:tictok_clone/features/authentication/login_screen.dart';
-import 'package:tictok_clone/features/authentication/username_screen.dart';
 import 'package:tictok_clone/features/authentication/widgets/auth_button.dart';
 
 class SignUpScreen extends StatelessWidget {
