ed2a6b829fd7d54855084907d8a52ab3f6aa49e4
lee gyuseung <gyus0421@gmail.com>
2024-04-02 23:04:58 +0900

    Tab Navigation : pushAndRemoveUntil

--- lib/features/onboarding/tutorial_screen.dart
+++ lib/features/onboarding/tutorial_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tictok_clone/constants/gaps.dart';
 import 'package:tictok_clone/constants/sizes.dart';
+import 'package:tictok_clone/features/onboarding/main_navigation/main_navigation_screen.dart';
 
 enum Direction { right, left }
 
@@ -42,6 +43,15 @@ class _TutorialScreenState extends State<TutorialScreen> {
         _showingPage = Page.first;
       });
     }
+  }
+
+  void _onEnterAppTap() {
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(
+        builder: (context) => const MainNavigationScreen(),
+      ),
+      (route) => false,
+    );
   }
 
   @override
@@ -119,7 +129,7 @@ class _TutorialScreenState extends State<TutorialScreen> {
               ),
               child: CupertinoButton(
                   color: Theme.of(context).primaryColor,
-                  onPressed: () {},
+                  onPressed: _onEnterAppTap,
                   child: const Text("Enter the app!")),
             ),
           ),
