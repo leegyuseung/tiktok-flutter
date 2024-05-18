77630fdccdb83151ba08386b5f63961e2703b43e
lee gyuseung <gyus0421@gmail.com>
2024-05-18 22:51:55 +0900

    SliverAppBar

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -13,16 +13,18 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
+          snap: true,
           floating: true,
-          stretch: true,
           pinned: true,
+          stretch: true,
           backgroundColor: Colors.teal,
           collapsedHeight: 80,
           expandedHeight: 200,
           flexibleSpace: FlexibleSpaceBar(
             stretchModes: const [
               StretchMode.blurBackground,
-              StretchMode.zoomBackground
+              StretchMode.zoomBackground,
+              StretchMode.fadeTitle
             ],
             background: Image.asset(
               "assets/images/placeholder.jpg",
@@ -30,6 +32,19 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             ),
             title: const Text('Hello!'),
           ),
+        ),
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text('Item $index'),
+              ),
+            ),
+          ),
+          itemExtent: 50,
         )
       ],
     );
