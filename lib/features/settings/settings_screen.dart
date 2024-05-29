7611174e35c0cbb1adc1a049ae7aee4896fabf6b
lee gyuseung <gyus0421@gmail.com>
2024-05-29 22:59:38 +0900

    Settings : showDateRangePicker

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -9,8 +9,25 @@ class SettingsScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Settings'),
         ),
-        body: const Column(
-          children: [],
+        body: ListView(
+          children: [
+            ListTile(
+              onTap: () => showAboutDialog(
+                context: context,
+                applicationVersion: "1.0",
+                applicationLegalese:
+                    "All rights reserved. Please don`t copy me.",
+              ),
+              title: const Text(
+                'About',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              subtitle: const Text('About this app...'),
+            ),
+            const AboutListTile()
+          ],
         ));
   }
 }
