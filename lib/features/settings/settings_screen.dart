3a7eca3b251c22a2de55936b8b4f670711efdd9b
lee gyuseung <gyus0421@gmail.com>
2024-06-02 21:07:54 +0900

    Settings : showDateRangePicker

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -6,28 +6,45 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Settings'),
-        ),
-        body: ListView(
-          children: [
-            ListTile(
-              onTap: () => showAboutDialog(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () async {
+              final date = await showDatePicker(
                 context: context,
-                applicationVersion: "1.0",
-                applicationLegalese:
-                    "All rights reserved. Please don`t copy me.",
-              ),
-              title: const Text(
-                'About',
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              subtitle: const Text('About this app...'),
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+              );
+              final time = await showTimePicker(
+                  context: context, initialTime: TimeOfDay.now());
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData(
+                      appBarTheme: const AppBarTheme(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                      ),
+                    ),
+                    child: child!,
+                  );
+                },
+              );
+            },
+            title: const Text(
+              'What is your birthday?',
             ),
-            const AboutListTile()
-          ],
-        ));
+          ),
+          const AboutListTile()
+        ],
+      ),
+    );
   }
 }
