e1bc8eac07116f9f0dbaf90588bc3b0b888c5d66
lee gyuseung <gyus0421@gmail.com>
2024-06-10 00:49:28 +0900

    Responsive Flutter Web : ConstrainedBox

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -41,10 +41,15 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
           scrolledUnderElevation: 1,
-          title: CupertinoSearchTextField(
-            controller: _textEditingController,
-            onChanged: _onSearchChanged,
-            onSubmitted: _onSearchSubmitted,
+          title: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: Breakpoints.sm,
+            ),
+            child: CupertinoSearchTextField(
+              controller: _textEditingController,
+              onChanged: _onSearchChanged,
+              onSubmitted: _onSearchSubmitted,
+            ),
           ),
           bottom: TabBar(
             tabAlignment: TabAlignment.start,
