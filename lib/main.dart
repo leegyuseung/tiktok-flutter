acc2065e34c2653646a290e287ab1187cccaad6f
lee gyuseung <gyus0421@gmail.com>
2024-06-18 23:59:09 +0900

    Dark Mode : Google Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -28,43 +28,19 @@ class TikTokApp extends StatelessWidget {
       title: 'TikTok Clone',
       themeMode: ThemeMode.system,
       darkTheme: ThemeData(
+        textTheme: GoogleFonts.nanumGothicTextTheme(
+          ThemeData(brightness: Brightness.dark).textTheme,
+        ),
         scaffoldBackgroundColor: Colors.black,
         primaryColor: const Color(0xFFE9435A),
         brightness: Brightness.dark,
       ),
       theme: ThemeData(
-        textTheme: TextTheme(
-          displayLarge: GoogleFonts.openSans(
-              fontSize: 96, fontWeight: FontWeight.w300, letterSpacing: -1.5),
-          displayMedium: GoogleFonts.openSans(
-              fontSize: 60, fontWeight: FontWeight.w300, letterSpacing: -0.5),
-          displaySmall:
-              GoogleFonts.openSans(fontSize: 48, fontWeight: FontWeight.w400),
-          headlineMedium: GoogleFonts.openSans(
-              fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-          headlineSmall:
-              GoogleFonts.openSans(fontSize: 24, fontWeight: FontWeight.w400),
-          titleLarge: GoogleFonts.openSans(
-              fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
-          titleMedium: GoogleFonts.openSans(
-              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
-          titleSmall: GoogleFonts.openSans(
-              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
-          bodyLarge: GoogleFonts.roboto(
-              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
-          bodyMedium: GoogleFonts.roboto(
-              fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-          labelLarge: GoogleFonts.roboto(
-              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
-          bodySmall: GoogleFonts.roboto(
-              fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
-          labelSmall: GoogleFonts.roboto(
-              fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
-        ),
         brightness: Brightness.light,
         // bottomAppBarTheme: BottomAppBarTheme(
         //   color: Colors.grey.shade50,
         // ),
+        textTheme: GoogleFonts.nanumGothicTextTheme(),
         textSelectionTheme: const TextSelectionThemeData(
           cursorColor: Color(0xFFE9435A),
         ),
